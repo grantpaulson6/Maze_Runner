aa59f65c9f81f52457cb3d11aeae96cca60770e4
Grant <grantpaulson6@gmail.com>
2019-07-17 18:16:49 -0700

    solution path saved

--- src/maze.js
+++ src/maze.js
@@ -19,17 +19,18 @@ class Maze {
         this.ladderImage = new Image();
         this.ladderImage.src = "../sprite_sheets/ladder.png";
 
-        this.rootNode = new Node(0,0);
+        this.rootNode = new Node(0,0,0,[]);
         const visited = new Set();
-
-        // this.pos = [25,25];
-        // this.ctx.rect(this.pos[0], this.pos[1], 50, 50);
-        // this.ctx.fill();
-
         this.d = 0;
+
+        this.path = [];
+        this.correctPath = [];
         this.last = [];
         this.nextNode(this.rootNode, visited,0);
-        console.log(this.last);
+
+        console.log(this.correctPath);
+        
+
         this.last[0] = this.last[0]*50+50;
         this.last[1] = this.last[1]*50+50;
         
@@ -80,37 +81,42 @@ class Maze {
         // this.rotateClockwiseBit(Math.PI/8);
     }
 
+    deepDup(arr) {
+        if (!Array.isArray(arr)) {
+            return arr;
+        }
+        const dupArr = [];
+        arr.forEach(el => dupArr.push(this.deepDup(el)));
+        return dupArr;
+    }
+
     nextNode(node, visited,d) {
         d++;
+        this.path.push([node.r,node.c]);
         if (d > this.d) {
             this.d = d;
             this.last = [node.r,node.c];
+            this.correctPath = this.deepDup(this.path);
         }
-        // window.setTimeout(()=>{
-
-            // this.ctx.rect(pos[1], pos[0], 50, 50);
-            // this.ctx.fill();
-            
-            visited.add(node.r +""+ node.c);
-            const neighbors = this.neighbors(node, visited);
-            this.shuffleArray(neighbors);
-            neighbors.forEach( n => {
-                if (!visited.has(n.r+""+n.c)) {
-                    if (n.d == 'd') {
-                        this.hWalls.delete(node.r + "" + node.c);
-                    } else if (n.d == 'u') {
-                        this.hWalls.delete(n.r + "" +  n.c);
-                    } else if (n.d == 'r') {
-                        this.vWalls.delete(node.r + "" +  node.c);
-                    } else if (n.d == 'l') {
-                        this.vWalls.delete(n.r + "" +  n.c);
-                    }
-                    node.next.push(n);
-                    // window.setTimeout(() => this.nextNode(n, visited).bind(this), 200);
-                    this.nextNode(n, visited, d);
+        visited.add(node.r +""+ node.c);
+        const neighbors = this.neighbors(node, visited);
+        this.shuffleArray(neighbors);
+        neighbors.forEach( n => {
+            if (!visited.has(n.r+""+n.c)) {
+                if (n.d == 'd') {
+                    this.hWalls.delete(node.r + "" + node.c);
+                } else if (n.d == 'u') {
+                    this.hWalls.delete(n.r + "" +  n.c);
+                } else if (n.d == 'r') {
+                    this.vWalls.delete(node.r + "" +  node.c);
+                } else if (n.d == 'l') {
+                    this.vWalls.delete(n.r + "" +  n.c);
                 }
-            });
-        // },200)
+                node.next.push(n);
+                this.nextNode(n, visited, d);
+            }
+        });
+        this.path.pop();
     }
 
     neighbors(node, visited) {
